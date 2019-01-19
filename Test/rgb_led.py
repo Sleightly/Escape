03cf3729f07ff435b20c1d341d5512bea8c3a0bd
Miranda Go <mirandago@Mirandas-MacBook-Pro-5.local>
2019-01-19 15:01:12 -0800

    added code to test rgb and microphone

---
+++ Test/rgb_led.py
@@ -0,0 +1,38 @@
+import mraa
+import time
+print (mraa.getVersion())
+
+red = mraa.Gpio(30)
+red.dir(mraa.DIR_OUT)
+red.write(0)
+
+green = mraa.Gpio(32)
+green.dir(mraa.DIR_OUT)
+green.write(0)
+
+blue = mraa.Gpio(34)
+blue.dir(mraa.DIR_OUT)
+blue.write(0)
+
+while True:
+    red.write(1)
+    time.sleep(5)
+    red.write(0)
+    green.write(1)
+    time.sleep(5)
+    green.write(0)
+    blue.write(1)
+    time.sleep(5)
+    blue.write(0)
+
+'''
+#Microphone
+mic = mraa.Gpio()
+mic.dir(mraa.DIR_IN)
+
+while True:
+    if int(mic.read()):
+        red.write(1)
+    else
+        red.write(0)
+'''
