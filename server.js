374e99b76f05b3fa2bdd2af55aff50c88b0b1470
Miranda Go <mirandago@Mirandas-MacBook-Pro-5.local>
2019-01-19 17:28:55 -0800

    setting up node server

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http'),
+    express = require('express'),
+    path = require('path');
+
+const PORT = process.env.PORT || 1500
+
+var app = express();
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+"/public/views", 'index.html'));
+})
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
