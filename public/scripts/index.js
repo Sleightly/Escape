033904e6b336584f404eb9d9091a97fe00145087
Miranda Go <mirandago@Mirandas-MacBook-Pro-5.local>
2019-01-19 19:37:43 -0800

    grid appears with correct input from height and width

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,5 +3,37 @@
     let id = req.session.lock;
     mod.getLockMembers(id, function(members) {res.send({members: members});});
   })*/
-var height = document.getElementById("height").value;
-console.log("this is the height: " + height);
+var height;
+var width;
+var canvas;
+var ctx;
+
+function getDimensions() {
+    height = document.getElementsByName("height")[0].value * 100;
+    width = document.getElementsByName("width")[0].value * 100;
+    console.log(height);
+    console.log(width);
+    displayBorders();
+}
+
+function displayBorders() {
+    canvas = document.getElementById("myCanvas");
+    canvas.height = height;
+    canvas.width = width;
+    displayLines();
+}
+
+function displayLines() {
+    ctx = canvas.getContext("2d");
+    for (var i = 0; i < height; i++) { 
+        ctx.moveTo((i+1)*100, 0);
+        ctx.lineTo((i+1)*100, height);
+        ctx.stroke();
+    }
+    for (var i = 0; i < width; i++) {
+        ctx.moveTo(0, (i+1)*100);
+        ctx.lineTo(width, (i+1)*100);
+        ctx.stroke();
+    }
+}
+
