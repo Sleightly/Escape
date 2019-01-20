0a497c91d0b9e0500ff7835f9ca5e2c55bffc416
Sleightly <ejc042@ucsd.edu>
2019-01-19 18:02:48 -0800

    fixed server again

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ app.use(express.static(__dirname));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+"/public/views", 'index.html'));
 })
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
